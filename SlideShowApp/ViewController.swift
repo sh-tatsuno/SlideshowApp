6478f41ae4bc1d1efbf533f83cebaca7a156e6ca
“sh-tatsuno;git config --global user.email “wasabi.mandarin@gmail.com <wasabi.mandarin@gmail.com>
2017-01-08 16:48:12 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by 龍野翔 on 2017/01/08.
+//  Copyright © 2017年 龍野翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
